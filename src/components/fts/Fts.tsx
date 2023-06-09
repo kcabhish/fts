add1a3822ea616313a09aab0a473021033cbf56c
kcabhish <kcabhish@gmail.com>
2023-06-09 09:07:28 -0400

    chore: placeholder for the fts app dashboard

--- src/components/fts/Fts.tsx
+++ src/components/fts/Fts.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import './fts.scss';
 
 export default function Fts() {
   return (
-    <div>Fts</div>
+    <div className='fts-main-container vertical-container'>
+        <div className='fts-header-container'>
+            buttons and status goes here
+        </div>
+        <div className='fts-body-container'>
+            this will render the spawned widgets. Should set the max threshod for the widget spawned.
+        </div>
+    </div>
   )
 }
--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import Card from '../components/Card/Card';
+import Card from '../components/Card/Card'
+
 
 export default function Playground() {
   return (
     <div className='horizontal-container'>
         <Card cardTitle='FTS' navigationLink='fts'></Card>
-        <Card cardTitle='FTS'></Card>
-        <Card cardTitle='FTS'></Card>
-        <Card cardTitle='FTS'></Card>
-        <Card cardTitle='FTS'></Card>
+        <Card cardTitle='Sample Card'></Card>
+        <Card cardTitle='Sample Card'></Card>
+        <Card cardTitle='Sample Card'></Card>
+        <Card cardTitle='Sample Card'></Card>
     </div>
   )
 }
