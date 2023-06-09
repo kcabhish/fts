eb2e57cee8ab81e870056f2b50d1ede7f23862c7
kcabhish <kcabhish@gmail.com>
2023-06-09 14:25:14 -0400

    chore: send message functionality added

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -1,17 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './widget.scss';
 
 interface IWidget {
     widgetTitle: string;
 }
 export default function Widget(props: IWidget) {
+  const [chatInputMsg, setChatInputMsg] = useState('');
+  // @todo: this needs to be redefined for translation in phase 2
+  const [messageList, setMessageList] = useState<string[]>([]);
+
+  /**
+   * Grabs contents from the message after the message is sent
+   * @param e 
+   */
+  const sendMsg = (e: any) => {
+    e.preventDefault();
+    const message = e.target.chatMsg.value
+    if (message) {
+        // add translation service here
+        setChatInputMsg('');
+        if (messageList.length > 0) {
+            setMessageList([...messageList, message]);
+        } else {
+            setMessageList([message]);
+        }
+    }
+  }
+
   return (
     <div className='widget-container'>
         <div className='widget-header'>
-            <h3>{props.widgetTitle}</h3>
+            <div className='widget-title'>
+                <h3>{props.widgetTitle}</h3>
+            </div>
+            <div className=''>
+                icons
+            </div>
+            
         </div>
-        <div className='widget-body'>Scrollable list of messages</div>
-        <div className='widget-footer'>Footer</div>
+        <div className='widget-body'>
+            {messageList.map(msg => {
+                // @TODO: need to update the key after using object for messages
+                return <p key={msg}>{msg}</p>
+            })}
+        </div>
+
+        <div className='widget-footer'>
+            <form name='send-msg-form' className='send-msg-form' onSubmit={sendMsg}>
+                <textarea name='chatMsg' value={chatInputMsg} onChange={(e) => setChatInputMsg(e.target.value)}></textarea>
+                <button>Send</button>
+            </form>
+        </div>
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Outlet, Link } from "react-router-dom";
 export default function Home() {
   return (
     <div>
-        <p>This is prototype for translating language during run time. Behing the back it utilizes Amazon translate service. When user types in the message in their selected language and sends the message,
+        <p>This is prototype for translating language during run time. Behind the back it utilizes Amazon translate service. When user types in the message in their selected language and sends the message,
         the inbound message on the other end will be translated to the language preference set on the widget.
         </p>
         <p>  
