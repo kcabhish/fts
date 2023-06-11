f06b209a558c10437a4d7c0c155ad77795502c49
kcabhish <kcabhish@gmail.com>
2023-06-11 09:39:55 -0400

    feat: translate route added

--- workspace/translateService/app.js
+++ workspace/translateService/app.js
@@ -8,10 +8,9 @@ const PORT = 3000;
 
 const errorController = require('./controllers/error');
 const app = express();
-const allRoutes = require('./routes/routes');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+const allRoutes = require('./routes/routes');
 app.use(allRoutes);
 app.use(errorController.get404);
 
--- workspace/translateService/routes/routes.js
+++ workspace/translateService/routes/routes.js
@@ -1,10 +1,37 @@
 const path = require('path');
 const express = require('express');
+require('dotenv').config();
 
 const router = express.Router();
 
 router.get('/isactive', (req, res, next) => {
     res.send('route is working');
+});
+
+router.get('/environments', (req, res, next) => {
+    res.send(process.env);
+});
+
+router.post('/translate', (req, res, next) => {
+    if (!req.body) {
+        res.status(400).send('check request body');
+    }
+    const {text, sourceCode, targetCode} = req.body;
+    const translatedText = text;
+    const responseBody = {
+        text, sourceCode, targetCode, translatedText
+    }
+    if (sourceCode === targetCode) {
+        res.send(responseBody);
+    } else {
+        console.log(text);
+        console.log(sourceCode);
+        console.log(targetCode);
+        // Translation should happen here
+        // parse the req body and use the translate method here
+        res.send(responseBody);
+    }
+
 });
 
 module.exports = router;
