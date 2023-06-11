35315026f4b86f9e1128cc7a8e4d79f9e30cd3eb
kcabhish <kcabhish@gmail.com>
2023-06-11 11:04:00 -0400

    fix: enable cors for service

--- workspace/translateService/app.js
+++ workspace/translateService/app.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors')
 const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
-const PORT = 3000;
+const PORT = 3001;
 
 const errorController = require('./controllers/error');
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 const allRoutes = require('./routes/routes');
